a260aee594bfec68c091aa665646b26b867486a4
Shahynkamali <shahynkamali@gmail.com>
2018-12-11 18:28:29 -0500

    Added scss for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Header.scss';
 
 function Header(props) {
 	return(
-		 <header>
+		 <header className="Header__Component">
           <h1>{props.title}</h1>
           <p>{props.message}</p>
         </header>
